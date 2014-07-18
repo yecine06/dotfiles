3ba4dab5f9eca838b2dce4c6e5ec239b6e872cc4
Yecine Dhouib <ydhouib@anypli.com>
2014-07-18 15:23:44 +0200

    Add install android sdk

---
+++ android/install.sh
@@ -0,0 +1,3 @@
+# Install android sdk
+# https://github.com/embarkmobile/android-sdk-installer
+curl -3L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | bash /dev/stdin --install=uild-tools-20.0.0,android-20,android-19,android-18,android-17 && source ~/.android-sdk-installer/env
---
+++ android/path.zsh
@@ -0,0 +1,3 @@
+# Add android tools in path
+export PATH="~/.android-sdk-installer/platform-tools:$PATH"
+export PATH="~/.android-sdk-installer/tools:$PATH"
