0746cf528cae8edf2a46ae290c3c28659784bc6d
Yecine Dhouib <ydhouib@anypli.com>
2014-07-19 13:40:12 +0200

    Changing path exported for android sdk

--- android/path.zsh
+++ android/path.zsh
@@ -1,3 +1,3 @@
 # Add android tools in path
-export PATH="~/.android-sdk-installer/platform-tools:$PATH"
-export PATH="~/.android-sdk-installer/tools:$PATH"
+export PATH="~/.android-sdk-installer/android-sdk-macosx/platform-tools:$PATH"
+export PATH="~/.android-sdk-installer/android-sdk-macosx/tools:$PATH"
