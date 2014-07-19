001e379c10ef61da10ee95ee7bc33a313b0512c5
Yecine Dhouib <ydhouib@anypli.com>
2014-07-19 13:38:29 +0200

    Add Pow server install and some gem tool for global setting
    Install gems like cocoapods, cloudapp ...

--- ruby/install.sh
+++ ruby/install.sh
@@ -11,3 +11,18 @@ then
   echo "  Installing ruby-build for you."
   brew install ruby-build > /tmp/ruby-build-install.log
 fi
+
+#Install Pow Server
+echo "  Installing Pow"
+curl get.pow.cx | sh > /tmp/pow-install.log
+
+#Install some gems
+echo "  Installing some gems"
+gem install powder
+gem install cloudapp
+gem install maid
+
+#Configuration some tools for ios development
+echo "  Installing some gems for ios development"
+gem install cocoapods
+gem install shenzhen
